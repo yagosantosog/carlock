418caf703c0a9b2a07741f4ffc56b6f9c9e0867e
Projetos Dev <projetosdev2026@gmail.com>
2025-12-14 19:59:42 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -28,6 +29,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'wonderful-cat-191f5294ba.media.strapiapp.com',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
